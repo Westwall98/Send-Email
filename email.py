535ee5a9e062f0688d5ea1ed5a7f59eea9d19f1c
Ethan <35027934+Westwall98@users.noreply.github.com>
2022-11-03 01:18:59 +0800

    Update email.py

--- email.py
+++ email.py
@@ -2,16 +2,11 @@ from openpyxl import load_workbook
 import win32com.client
 
 def findlist():
-
     wb = load_workbook('C:\\Users\\Ethan\\Desktop\\Project\\user list.xlsx')
     ws = wb.active
-
     rows_data = list(ws.rows)
-
     titles = [title.value for title in rows_data.pop(0)]
-
     all_row_dict = []
-
     for a_row in rows_data:
         the_row_data = [cell.value for cell in a_row]
         row_dict = dict(zip(titles, the_row_data))
@@ -23,21 +18,22 @@ def findlist():
 def outlook(count):
     userlist = findlist()
     outlook = win32com.client.Dispatch("Outlook.Application")
+    # outlook = win32com.client.DispatchEx("Outlook.Application")
+    # outlook.Visible = 0
     msg = outlook.GetNamespace("MAPI").OpenSharedItem(r"C:\\Users\\Ethan\\Desktop\\Project\\Check-in 2022 - Basic.msg")
     for i in range(count):
         firstname = userlist[i]['User First Name']
         useremailaddr = userlist[i]['Email']
         msg.HTMLBody = msg.HTMLBody.replace('%%FirstName%%','{},'.format(firstname))
-
         mail = outlook.CreateItem(0)
-
         mail.HTMLBody = msg.HTMLBody
         mail.To = useremailaddr
         mail.Subject = '有关IT的支持，可以联系我们！'
         mail.BodyFormat = 2  # 2表示使用Html format
+        # mail.Attachments.Add('C:\Users\xxx\Desktop\git_auto_pull_new.py')
 
-        mail.display()  #显示发送邮件界面
-        # mail.send()
+        mail.Display()
+        # mail.Send()
 
 
 outlook(1)
